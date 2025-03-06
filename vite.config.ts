6f843578d3b3cbee6af37f0785a06a474f339da9
Eduardo Braam <53708118+SrBraam@users.noreply.github.com>
2025-03-06 09:38:38 -0300

    Update vite.config.ts
    removed tempo

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { tempo } from "tempo-devtools/dist/vite";
-
-// https://vitejs.dev/config/
-// Add this block of code
-const conditionalPlugins = [];
-if (process.env.TEMPO === "true") {
-  conditionalPlugins.push("tempo-devtools/dist/babel-plugin");
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [...conditionalPlugins],
-      },
-    }),
-    tempo(),
-  ],
-  server: {
-    allowedHosts: process.env.TEMPO === "true" ? true : undefined,
-  },
-  optimizeDeps: {
-    exclude: ["lucide-react"],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
   },
 });
